b13337dd75f8e71bb7fa47de9e9722dbdce0c983
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-22 12:32:59 +0200

    Split projects into separate Command and Query applications does not build

---
+++ inventory-query/src/main/java/ua/dp/maxym/demo8/inventory/Demo8AxonInventoryApplication.java
@@ -0,0 +1,13 @@
+package ua.dp.maxym.demo8.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo8AxonInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo8AxonInventoryApplication.class, args);
+    }
+
+}
---
+++ user-query/src/main/java/ua/dp/maxym/demo8/user/Demo8AxonUserApplication.java
@@ -0,0 +1,13 @@
+package ua.dp.maxym.demo8.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo8AxonUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo8AxonUserApplication.class, args);
+    }
+
+}
