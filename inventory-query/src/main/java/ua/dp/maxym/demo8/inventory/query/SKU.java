754994fd51c7448e3596a27b75e669b61f8b6ef0
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-24 16:22:44 +0200

    Adding Project Lombok -105 lines of code :)

--- inventory-query/src/main/java/ua/dp/maxym/demo8/inventory/query/SKU.java
+++ inventory-query/src/main/java/ua/dp/maxym/demo8/inventory/query/SKU.java
@@ -2,83 +2,29 @@ package ua.dp.maxym.demo8.inventory.query;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class SKU {
 
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
     private Warehouse warehouse;
     private String skuName;
     private Integer quantity;
     private Double pricePerItem;
+
     public SKU() {
     }
+
     public SKU(Warehouse warehouse, String skuName, Integer quantity, Double pricePerItem) {
         this.warehouse = warehouse;
         this.skuName = skuName;
         this.quantity = quantity;
         this.pricePerItem = pricePerItem;
     }
-
-    @SuppressWarnings("unused")
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
-    public Warehouse getWarehouse() {
-        return warehouse;
-    }
-
-    @SuppressWarnings("unused")
-    public void setWarehouse(Warehouse warehouse) {
-        this.warehouse = warehouse;
-    }
-
-    @Override
-    public String toString() {
-        return "SKU{" +
-                "id=" + id +
-                ", skuName='" + skuName + '\'' +
-                ", quantity=" + quantity +
-                ", pricePerItem=" + pricePerItem +
-                '}';
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @SuppressWarnings("unused")
-    public String getSkuName() {
-        return skuName;
-    }
-
-    @SuppressWarnings("unused")
-    public void setSkuName(String skuName) {
-        this.skuName = skuName;
-    }
-
-    @SuppressWarnings("unused")
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    @SuppressWarnings("unused")
-    public Double getPricePerItem() {
-        return pricePerItem;
-    }
-
-    @SuppressWarnings("unused")
-    public void setPricePerItem(Double pricePerItem) {
-        this.pricePerItem = pricePerItem;
-    }
-
 }
--- inventory-query/src/main/java/ua/dp/maxym/demo8/inventory/query/Warehouse.java
+++ inventory-query/src/main/java/ua/dp/maxym/demo8/inventory/query/Warehouse.java
@@ -4,77 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
+@Data
 public final class Warehouse {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "warehouse")
     private Set<SKU> skus = new HashSet<>();
+
+    public Warehouse() {
+    }
 
     public Warehouse(String name) {
         this.name = name;
     }
 
-    public Warehouse() {
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @SuppressWarnings("unused")
-    public String getName() {
-        return name;
-    }
-
-    @SuppressWarnings("unused")
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @OneToMany(mappedBy = "warehouse")
-    public Set<SKU> getSkus() {
-        return skus;
-    }
-
-    @SuppressWarnings("unused")
-    public void setSkus(Set<SKU> skus) {
-        this.skus = skus;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Warehouse warehouse = (Warehouse) o;
-
-        if (!id.equals(warehouse.id)) return false;
-        if (!name.equals(warehouse.name)) return false;
-        return skus.equals(warehouse.skus);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, skus);
-    }
-
-    @Override
-    public String toString() {
-        return "Warehouse{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", skus=" + skus +
-                '}';
-    }
 }
