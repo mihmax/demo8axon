42cb91d3bfca02ecf3d0b0bb636230352fd88684
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-24 16:41:18 +0200

    Adding Project Lombok -61 lines of code :)

--- user-query/src/main/java/ua/dp/maxym/demo8/user/query/User.java
+++ user-query/src/main/java/ua/dp/maxym/demo8/user/query/User.java
@@ -3,11 +3,13 @@ package ua.dp.maxym.demo8.user.query;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
-import java.util.Objects;
+import lombok.Data;
 
 @Entity(name = "userProjection") // "user" is reserved keyword in H2
+@Data
 public final class User {
+    @Id
+    @GeneratedValue
     private Long id;
     private String email;
     private String firstName;
@@ -22,71 +24,5 @@ public final class User {
     }
 
     public User() {
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @SuppressWarnings("unused")
-    public String getEmail() {
-        return email;
-    }
-
-    @SuppressWarnings("unused")
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @SuppressWarnings("unused")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @SuppressWarnings("unused")
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @SuppressWarnings("unused")
-    public String getLastName() {
-        return lastName;
-    }
-
-    @SuppressWarnings("unused")
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @SuppressWarnings("unused")
-    public Double getMoney() {
-        return money;
-    }
-
-    public void setMoney(Double money) {
-        this.money = money;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id)
-                && Objects.equals(email, user.email)
-                && Objects.equals(firstName, user.firstName)
-                && Objects.equals(lastName, user.lastName) && Objects.equals(
-                money, user.money);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, firstName, lastName, money);
     }
 }
