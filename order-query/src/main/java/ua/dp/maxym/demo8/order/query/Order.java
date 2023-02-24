5abe2cc4d600fc414ef21c59f2904dccb937625e
Maxym Mykhalchuk <mihmax@gmail.com>
2023-02-24 16:36:55 +0200

    Adding Project Lombok -146 lines of code :)

--- order-query/src/main/java/ua/dp/maxym/demo8/order/query/Order.java
+++ order-query/src/main/java/ua/dp/maxym/demo8/order/query/Order.java
@@ -4,16 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity(name = "orderProjection") // "order" is reserved keyword in H2
+@Data
 public final class Order {
+    @Id
+    @GeneratedValue
     private Long id;
     private String orderId;
     private String userId;
+    @OneToMany(mappedBy = "order")
     private Set<OrderItem> orderItems = new HashSet<>();
     private Double total;
     private String state;
@@ -28,89 +32,5 @@ public final class Order {
     }
 
     public Order() {
-    }
-
-    @SuppressWarnings("unused")
-    public String getOrderId() {
-        return orderId;
-    }
-
-    @SuppressWarnings("unused")
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    @SuppressWarnings("unused")
-    public String getUserId() {
-        return userId;
-    }
-
-    @SuppressWarnings("unused")
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    @SuppressWarnings("unused")
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    @SuppressWarnings("unused")
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    @SuppressWarnings("unused")
-    public String getRejectionReason() {
-        return rejectionReason;
-    }
-
-    public void setRejectionReason(String rejectionReason) {
-        this.rejectionReason = rejectionReason;
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @OneToMany(mappedBy = "order")
-    public Set<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    @SuppressWarnings("unused")
-    public void setOrderItems(Set<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return Objects.equals(id, order.id) && Objects.equals(orderId,
-                                                              order.orderId) && Objects.equals(
-                userId, order.userId) && Objects.equals(orderItems, order.orderItems) && Objects.equals(
-                total, order.total) && Objects.equals(state, order.state) && Objects.equals(
-                rejectionReason, order.rejectionReason);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, orderId, userId, orderItems, total, state, rejectionReason);
     }
 }
--- order-query/src/main/java/ua/dp/maxym/demo8/order/query/OrderItem.java
+++ order-query/src/main/java/ua/dp/maxym/demo8/order/query/OrderItem.java
@@ -2,11 +2,17 @@ package ua.dp.maxym.demo8.order.query;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class OrderItem {
 
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
     private Order order;
     private String skuName;
     private Integer quantity;
@@ -19,47 +25,4 @@ public class OrderItem {
         this.skuName = skuName;
         this.quantity = quantity;
     }
-
-    @SuppressWarnings("unused")
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
-    public Order getOrder() {
-        return order;
-    }
-
-    @SuppressWarnings("unused")
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @SuppressWarnings("unused")
-    public String getSkuName() {
-        return skuName;
-    }
-
-    @SuppressWarnings("unused")
-    public void setSkuName(String skuName) {
-        this.skuName = skuName;
-    }
-
-    @SuppressWarnings("unused")
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    @SuppressWarnings("unused")
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
 }
